36b0ec35aeb6c17164b5560ccb30b765ce9f04ac
Razhatawka <nataliia85sakhnova@gmail.com>
2022-09-06 17:56:55 +0300

    готово

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // Сделать локальное хранилище заметок для себя любимой. Не забыть валидацию при добавлении заметки.
 
-//Нажатие на кнопку -> событие -> value inputa записывается в <ul>
-// нажатие на другую кнопку -> событие -> все ulы удаляются
-// надо еще по отдельной кнопке к каждому добавляемому ulу, чтоб их отдельно удалять можно было
-
 const button_add = document.getElementById("button_add"); //кнопка добавления
 const button_delete = document.getElementById("button_delete"); //кнопка удаления
 const form = document.getElementById("form"); //форма ввода
@@ -22,26 +18,14 @@ const addNote = () => {
         li.appendChild(privat_button); // done, добавляем кнопку для удаления конкретной заметки
         privat_button.classList.add("privat_button"); // done, добавляем класс кнопке
 
-        const buttons = document.querySelectorAll(".privat_button") //done, создается массив кнопок
-        buttons.forEach((e) => e.addEventListener("click", (e) => {
-                e.target.parentNode.remove(); //done, заметка удаляется
-            }))
-    return true;
-
-
-
-} else {
-    document.getElementById('cont').innerHTML = 'Пустые заметки не сохраняются';
-
-    if (document.getElementById('cont').innerHTML != "")
-    // напишу-ка я функцию, которая ко всем кнопкам на странице присобачивает одно и то же действие:
-    // document.getElementsByClassName("buttons") - произошел клик
-    // или document.getElementsByClassName("button_add") - произошел клик
-    // или document.getElementsByClassName("button_delete") - произошел клик
-    // то тогда:
-    // document.getElementById('cont').innerHTML = '';
-    // return false;
-}
+        privat_button.addEventListener("click", (e) => {
+            e.target.parentNode.remove(); //done, заметка удаляется
+        })
+        document.getElementById('cont').innerHTML = '';
+        return true;
+    } else {
+        document.getElementById('cont').innerHTML = 'Пустые заметки не сохраняются';
+    }
 }
 
 const deleteAllNotes = () => {
